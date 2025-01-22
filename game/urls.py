151d66d2423b77d1c38fc9565399002ac38562d5
deividmng <mangassk8@gmail.com>
2025-01-22 13:48:29 +0000

    game now is works well, and the button to restart the game

--- game/urls.py
+++ game/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.hello),# el  '' esta vacio para que sea la primera vista 
     path('about/', views.about),# el  '' esta vacio para que sea la primera vista 
-    path('hello/', views.hello),# el  '' esta vacio para que sea la primera vista 
-    path('projects/', views.project_list, name='project_list'),
+    path('hello/', views.hello, name='hello'),
+    path('question/<int:question_id>/', views.hello, name='hello_with_id'), 
+    path('reset_score/', views.reset_score, name='reset_score'), 
+    # path('projects/', views.project_list, name='project_list'),
     
 ]
